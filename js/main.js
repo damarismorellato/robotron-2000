f246271a5abe3b44afcf97488a8124c4ad61f1a5
damarismorellato <dls.morellato19@gmail.com>
2022-11-22 06:30:55 -0300

    Adicionando soma e subtração

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 const robotron = document.querySelector('#robotron');
 
-robotron.addEventListener('click', dizOi);
 
-function dizOi() {
-    console.log('oi');
-}
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
 
+somar.addEventListener('click', (evento) => {
+    braco.value = parseInt(braco.value) + 1;
+    console.log('cliquei');
+});
+
+subtrair.addEventListener('click', (evento) => {
+    braco.value = parseInt(braco.value) - 1;
+});
